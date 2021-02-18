777a04f083db1dd1acff5fc2edbac7d08e4732ea
Aigerim Tumbayeva <aigerimt922@gmail.com>
2021-02-18 20:09:28 +0600

    add_junit patch applied

--- src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -1,23 +1,28 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Repository;
-import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.UserTestData;
+
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+
 import java.util.stream.Collectors;
+import static ru.javawebinar.topjava.UserTestData.admin;
+import static ru.javawebinar.topjava.UserTestData.user;
 
 @Repository
 public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
 
 
-    static final int USER_ID = 1;
-    static final int ADMIN_ID = 2;
+
+        public void init() {
+                map.clear();
+                map.put(UserTestData.USER_ID, user);
+                map.put(UserTestData.ADMIN_ID, admin);
+            }
 
     @Override
     public List<User> getAll() {
