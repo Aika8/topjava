1da96d3198d0622e490f058d8ace962cec5c7de6
admin@javaops.ru <gitlaB13>
2021-02-25 00:02:26 +0300

    4_5_improve_code

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -1,47 +1,34 @@
 package ru.javawebinar.topjava.model;
 
-import org.hibernate.Hibernate;
 import org.springframework.util.Assert;
-import ru.javawebinar.topjava.HasId;
 
-import javax.persistence.*;
-
-@MappedSuperclass
-// http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
-@Access(AccessType.FIELD)
-//@JsonAutoDetect(fieldVisibility = ANY, getterVisibility = NONE, isGetterVisibility = NONE, setterVisibility = NONE)
-public abstract class AbstractBaseEntity implements HasId {
+public abstract class AbstractBaseEntity {
     public static final int START_SEQ = 100000;
 
-    @Id
-    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
-    //    @Column(name = "id", unique = true, nullable = false, columnDefinition = "integer default nextval('global_seq')")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
-//  See https://hibernate.atlassian.net/browse/HHH-3718 and https://hibernate.atlassian.net/browse/HHH-12034
-//  Proxy initialization when accessing its identifier managed now by JPA_PROXY_COMPLIANCE setting
     protected Integer id;
 
-    protected AbstractBaseEntity() {
+    public AbstractBaseEntity() {
     }
 
     protected AbstractBaseEntity(Integer id) {
         this.id = id;
     }
 
-    @Override
     public void setId(Integer id) {
         this.id = id;
     }
 
-    @Override
     public Integer getId() {
         return id;
     }
 
-    // doesn't work for hibernate lazy proxy
     public int id() {
         Assert.notNull(id, "Entity must have id");
         return id;
+    }
+
+    public boolean isNew() {
+        return this.id == null;
     }
 
     @Override
@@ -54,7 +41,7 @@ public abstract class AbstractBaseEntity implements HasId {
         if (this == o) {
             return true;
         }
-        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
         AbstractBaseEntity that = (AbstractBaseEntity) o;
