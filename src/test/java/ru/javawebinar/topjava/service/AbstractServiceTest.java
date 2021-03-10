edbe7509a891b2ad1f2d5d5aabde2c845688cddd
admin@javaops.ru <gitlaB13>
2021-03-10 14:13:43 +0300

    6_03_extract_rules

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,34 +1,29 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
-import ru.javawebinar.topjava.TimingExtension;
+import ru.javawebinar.topjava.TimingRules;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
-
-@SpringJUnitConfig(locations = {
+@ContextConfiguration({
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
 })
-//@ExtendWith(SpringExtension.class)
-@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+@RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ExtendWith(TimingExtension.class)
-public abstract class AbstractServiceTest {
+@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+abstract public class AbstractServiceTest {
+    @ClassRule
+    public static ExternalResource summary = TimingRules.SUMMARY;
 
-    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
-    protected <T extends Throwable> void validateRootCause(Class<T> rootExceptionClass, Runnable runnable) {
-        assertThrows(rootExceptionClass, () -> {
-            try {
-                runnable.run();
-            } catch (Exception e) {
-                throw getRootCause(e);
-            }
-        });
-    }
+    @Rule
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
 }
