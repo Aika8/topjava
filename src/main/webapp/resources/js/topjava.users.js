830d252c8ab79ee997b122ca6185946cdb8009f5
admin@javaops.ru <gitlaB13>
2021-04-01 12:54:21 +0300

    8_09_ajax_datatables

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -1,74 +1,34 @@
-const userAjaxUrl = "admin/users/";
-
-// https://stackoverflow.com/a/5064235/548473
-const ctx = {
-    ajaxUrl: userAjaxUrl,
-    updateTable: function () {
-        $.get(userAjaxUrl, updateTableByData);
-    }
-}
-
-function enable(chkbox, id) {
-    var enabled = chkbox.is(":checked");
-//  https://stackoverflow.com/a/22213543/548473
-    $.ajax({
-        url: userAjaxUrl + id,
-        type: "POST",
-        data: "enabled=" + enabled
-    }).done(function () {
-        chkbox.closest("tr").attr("data-userEnabled", enabled);
-        successNoty(enabled ? "common.enabled" : "common.disabled");
-    }).fail(function () {
-        $(chkbox).prop("checked", !enabled);
-    });
-}
+const ajaxUrl = "admin/users/";
+let datatableApi;
 
 // $(document).ready(function () {
 $(function () {
-    makeEditable({
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
         "columns": [
             {
                 "data": "name"
             },
             {
-                "data": "email",
-                "render": function (data, type, row) {
-                    if (type === "display") {
-                        return "<a href='mailto:" + data + "'>" + data + "</a>";
-                    }
-                    return data;
-                }
+                "data": "email"
             },
             {
                 "data": "roles"
             },
             {
-                "data": "enabled",
-                "render": function (data, type, row) {
-                    if (type === "display") {
-                        return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='enable($(this)," + row.id + ");'/>";
-                    }
-                    return data;
-                }
+                "data": "enabled"
             },
             {
-                "data": "registered",
-                "render": function (date, type, row) {
-                    if (type === "display") {
-                        return date.substring(0, 10);
-                    }
-                    return date;
-                }
+                "data": "registered"
             },
             {
-                "orderable": false,
-                "defaultContent": "",
-                "render": renderEditBtn
+                "defaultContent": "Edit",
+                "orderable": false
             },
             {
-                "orderable": false,
-                "defaultContent": "",
-                "render": renderDeleteBtn
+                "defaultContent": "Delete",
+                "orderable": false
             }
         ],
         "order": [
@@ -76,11 +36,7 @@ $(function () {
                 0,
                 "asc"
             ]
-        ],
-        "createdRow": function (row, data, dataIndex) {
-            if (!data.enabled) {
-                $(row).attr("data-userEnabled", false);
-            }
-        }
+        ]
     });
+    makeEditable();
 });
